35b72686ebef7f9338357703db7c8dc37fbfba50
verbeen <l_puddu@hotmail.com>
2014-03-28 09:53:37 +0100

    MovementServer -> MovementService Small test with ZMQ and a queue device.

---
+++ MovementService/src/main/java/Start.java
@@ -0,0 +1,12 @@
+import service.Server;
+
+/**
+ * Created by geh on 26-3-14.
+ */
+public class Start
+{
+    public static void main(String[] args)
+    {
+        new Server();
+    }
+}
--- MovementService/src/main/java/service/Server.java
+++ MovementService/src/main/java/service/Server.java
@@ -1,32 +1,63 @@
 package service;
 
-import org.zeromq.ZMQ;
+import org.zeromq.*;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+import org.zeromq.ZMQQueue;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by geh on 21-3-14.
  */
 public class Server
 {
-    public static void main(String[] args)
+    private ZMQQueue queueDevice;
+    private List<Worker> workers;
+
+    public Server()
     {
         String serverName = "billservice";
         String routerAddress = "tcp://" + serverName + ":32500";
         String dealerAddress = "inproc://responders";
 
-        ZMQ.Context ctx = ZMQ.context(1);
-        ZMQ.Socket routerSocket = ctx.socket(ZMQ.ROUTER);
-        ZMQ.Socket dealerSocket = ctx.socket(ZMQ.DEALER);
-        routerSocket.bind(routerAddress);
-        dealerSocket.bind(dealerAddress);
+        Context ctx = ZMQ.context(1);
+        Socket routerSocket = ctx.socket(ZMQ.ROUTER);
+        Socket dealerSocket = ctx.socket(ZMQ.DEALER);
 
-        ArrayList<Worker> repliers = new ArrayList<Worker>();
+        this.queueDevice = new ZMQQueue(ctx, routerSocket, dealerSocket);
+        this.workers = new ArrayList<Worker>();
+
         for(int i = 0; i < 10; i++)
         {
             Worker worker = new Worker(ctx, dealerAddress);
-            repliers.add(worker);
+            this.workers.add(worker);
+        }
+    }
+
+    public void start()
+    {
+        this.queueDevice.run();
+        for(Worker worker : this.workers)
+        {
             worker.start();
+        }
+    }
+
+    public void stop()
+    {
+        try
+        {
+            this.queueDevice.close();
+            for(Worker worker : this.workers)
+            {
+                worker.stop();
+            }
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
         }
 
     }
