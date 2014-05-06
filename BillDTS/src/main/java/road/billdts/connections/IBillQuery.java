bcbd83c21520a96684aa662a96b843d674bd65d6
verbeen <l_puddu@hotmail.com>
2014-05-06 10:14:21 +0200

    renamed client connections. added unversioned files. why unversioned? only git knows.

---
+++ BillDTS/src/main/java/road/billdts/connections/IBillQuery.java
@@ -0,0 +1,12 @@
+package road.billdts.connections;
+
+import aidas.userservice.dto.UserDto;
+
+/**
+ * Created by geh on 11-4-14.
+ * This interface specifies all the possible queries that can be done from the BillSystem.
+ */
+public interface IBillQuery
+{
+    UserDto authenticate(String user, String password);
+}
