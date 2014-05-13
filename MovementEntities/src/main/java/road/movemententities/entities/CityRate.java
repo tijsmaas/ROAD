386bc92bf5dcddbe5dc4cb7478c83f8bdbb0f196
YamiNoSenshi <mitch.van.bijsterveld@gmail.com>
2014-05-13 12:13:12 +0200

    Entity voor kilometer rate.

---
+++ MovementEntities/src/main/java/road/movemententities/entities/CityRate.java
@@ -0,0 +1,44 @@
+package road.movemententities.entities;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import java.util.Date;
+
+/**
+ * Created by Mitch on 12-5-2014.
+ */
+@Entity
+public class CityRate
+{
+    @EmbeddedId
+    @Column(unique = false, nullable = true)
+    CityRateId id;
+
+    @Column(unique = false, nullable = true)
+    private String kilometerRate;
+
+    public CityRate(){}
+
+    public CityRate(String cityId, Date addDate, String kilometerRate)
+    {
+
+        this.kilometerRate = kilometerRate;
+    }
+
+    public CityRateId getId() {
+        return id;
+    }
+
+    public void setId(CityRateId id) {
+        this.id = id;
+    }
+
+    public String getKilometerRate() {
+        return kilometerRate;
+    }
+
+    public void setKilometerRate(String kilometerRate) {
+        this.kilometerRate = kilometerRate;
+    }
+}
