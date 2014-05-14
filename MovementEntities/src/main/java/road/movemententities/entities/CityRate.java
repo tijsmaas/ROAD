4818a0ac6e2b0786c64a5715ddeb2418a4142dbf
YamiNoSenshi <mitch.van.bijsterveld@gmail.com>
2014-05-15 01:50:13 +0200

    Entities bijgewerkt.

--- MovementEntities/src/main/java/road/movemententities/entities/CityRate.java
+++ MovementEntities/src/main/java/road/movemententities/entities/CityRate.java
@@ -1,39 +1,49 @@
 package road.movemententities.entities;
 
-import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Date;
 
 /**
  * Created by Mitch on 12-5-2014.
  */
 @Entity
+@IdClass(value=CityRateId.class)
 public class CityRate
 {
-    @EmbeddedId
-    @Column(unique = false, nullable = true)
-    CityRateId id;
+    @Id
+    private City city;
 
-    @Column(unique = false, nullable = true)
+    @Id
+    private Date addDate;
+
     private String kilometerRate;
 
     public CityRate(){}
 
     public CityRate(City city, Date addDate, String kilometerRate)
     {
-        id = new CityRateId();
+        /*id = new CityRateId();
         id.setCity(city);
-        id.setAddDate(addDate);
+        id.setAddDate(addDate);*/
+        this.city = city;
+        this.addDate = addDate;
         this.kilometerRate = kilometerRate;
     }
 
-    public CityRateId getId() {
-        return id;
+    public City getCity() {
+        return city;
     }
 
-    public void setId(CityRateId id) {
-        this.id = id;
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Date getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(Date addDate) {
+        this.addDate = addDate;
     }
 
     public String getKilometerRate() {
