d0c5d7e1a8071788e1593e57d0acbefe683eb07f
niekert <niekkruse70@gmail.com>
2014-05-19 23:39:18 +0200

    Removed even more unnecessary methods

--- MovementMapper/src/main/java/road/movementmapper/ParserStartup.java
+++ MovementMapper/src/main/java/road/movementmapper/ParserStartup.java
@@ -1,8 +1,6 @@
 package road.movementmapper;
 
 import org.xml.sax.SAXException;
-import road.movemententities.entities.*;
-import road.movemententities.entities.enumerations.PaymentStatus;
 import road.movementmapper.dao.EntityDAO;
 import road.movementmapper.dao.MovementsDAO;
 import road.movementparser.injectable.MovementParser;
@@ -13,10 +11,6 @@ import javax.ejb.Startup;
 import javax.inject.Inject;
 import java.io.File;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -77,88 +71,5 @@ public class ParserStartup
     @Inject
     private MovementsDAO movementsDAO;
 
-    public void generateTestUsers()
-    {
-        String cartrackerID = "t0";
-        String[] cartrackerIDs =
-        {
-            "t0", "t1", "t3", "tq2", "tt4"
-        };
-        String[] licensePlates =
-        {
-            "07-fr-fg", "30-STS-8", "61-rf-rz", "39-tk-nd", "86-sz-nx", "51-fs-jd", "tj-sg-03"
-        };
-
-        for (int i = 0; i < 5; i++)
-        {
-            // Create vehicle ownership of car t0
-            GregorianCalendar registerdate = new GregorianCalendar();
-            registerdate.add(Calendar.YEAR, -3);
-            GregorianCalendar expdate = new GregorianCalendar();
-            expdate.add(Calendar.YEAR, 3);
-            VehicleOwnership vehicleOwnership = new VehicleOwnership();
-            vehicleOwnership.setContributeGPSData(true);
-            vehicleOwnership.setRegistrationdate(registerdate);
-            vehicleOwnership.setRegistrationExperationDate(expdate);
-            vehicleOwnership.setUserID(i);
-            Vehicle car_t0 = (Vehicle) movementsDAO.getOrCreateVehicleById(cartrackerIDs[i]);
-            car_t0.setLicensePlate(licensePlates[i]);
-            vehicleOwnership.setVehicle(car_t0);
-            entityDAO.create(vehicleOwnership);
-        }
-    }
-
-    /**
-     * Generate 1 example invoice
-     */
-    public void generateTestData()
-    {
-        int userID = 1;
-        String cartrackerID = "t0";
-
-        // Create vehicle ownership of car t0
-        GregorianCalendar registerdate = new GregorianCalendar();
-        registerdate.add(Calendar.YEAR, -3);
-        GregorianCalendar expdate = new GregorianCalendar();
-        expdate.add(Calendar.YEAR, 3);
-        VehicleOwnership vehicleOwnership = new VehicleOwnership();
-        vehicleOwnership.setContributeGPSData(true);
-        vehicleOwnership.setRegistrationdate(registerdate);
-        vehicleOwnership.setRegistrationExperationDate(expdate);
-        vehicleOwnership.setUserID(userID);
-        Vehicle t0 = (Vehicle) movementsDAO.getOrCreateVehicleById(cartrackerID);
-        vehicleOwnership.setVehicle(t0);
-        entityDAO.create(vehicleOwnership);
-
-        // Generate vehicle city visits
-        List<CityDistance> cityDistances = new ArrayList();
-        City woenselnoord = (City) entityDAO.findById(City.class, "-8");
-        cityDistances.add(new CityDistance(woenselnoord, 40.0, 1.25));
-        City geldrop = (City) entityDAO.findById(City.class, "-40");
-        cityDistances.add(new CityDistance(geldrop, 10.0, 0.25));
-        City tilburg = (City) entityDAO.findById(City.class, "-10");
-        cityDistances.add(new CityDistance(tilburg, 30.0, 2.25));
-        entityDAO.create(vehicleOwnership);
-
-        // Generate VehicleInvoices
-        VehicleInvoice vehicleInvoice = new VehicleInvoice();
-        vehicleInvoice.setOwnership(vehicleOwnership);
-        vehicleInvoice.setMovementList(cityDistances);
-        entityDAO.create(vehicleInvoice);
-
-        // Generate invoice
-        Calendar startdate = Calendar.getInstance();
-        startdate.add(Calendar.YEAR, -3);
-        Calendar enddate = Calendar.getInstance();
-        enddate.add(Calendar.YEAR, 3);
-        Calendar gendate = Calendar.getInstance();
-        gendate.set(2013, 04, 1);
-        Invoice invoice = new Invoice();
-        invoice.setStartDate(startdate.getTime());
-        invoice.setEndDate(enddate.getTime());
-        invoice.setPaymentStatus(PaymentStatus.NOT_PAID);
-        invoice.setGenerationDate(gendate.getTime());
-        entityDAO.create(invoice);
-    }
 
 }
